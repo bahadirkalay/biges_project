c4c52aa4c6cc1152af1505e6b9706d0221bae3ff
Bahadir KALAY <bahadir@ovoanalytics.com>
2021-02-21 00:47:59 +0300

    Admin sayfasına kayıt yapılmıştır. Test verilerin girişi yapılmıştır.

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from product.models import GameModel, CategoryModel
 # Register your models here.
+
+@admin.register(GameModel)
+class ArticleAdmin(admin.ModelAdmin):
+    search_fields = ("name", 'desc')
+    list_display = ("name", 'year',)
+
+@admin.register(CategoryModel)
+class ArticleAdmin(admin.ModelAdmin):
+    search_fields = ("title", 'desc')
+    list_display = ("title",)
+
--- product/models/game_model.py
+++ product/models/game_model.py
@@ -4,15 +4,9 @@ import datetime
 from product.models import CategoryModel
 # Create your models here.
 
-def year_choices():
-    return [(r,r) for r in range(1984, datetime.date.today().year+1)]
-
-def current_year():
-    return datetime.date.today().year
-
 class GameModel(models.Model):
-    name = models.CharField(max_length=80, blank=False, null=False verbose_name="Oyunun Adı")
-    year = models.IntegerField(('year'), choices=year_choices, default=current_year , verbose_name="Yıl")
+    name = models.CharField(max_length=80, blank=False, null=False ,verbose_name="Oyunun Adı")
+    year = models.IntegerField(verbose_name="Yıl")
     category = models.ManyToManyField(CategoryModel , verbose_name="Kategori Adı")
     slug = AutoSlugField(populate_from="name", unique=True)
 
